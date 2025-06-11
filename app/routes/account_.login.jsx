e664d56d7a565e91aa1985ffeaa6182d3dc73611
Ahmed Khalaf <ahmedmohamadkhalaf@gmail.com>
2025-06-11 18:59:01 +0200

    build login func

--- app/routes/account_.login.jsx
+++ app/routes/account_.login.jsx
@@ -2,7 +2,33 @@
  * @param {LoaderFunctionArgs}
  */
 export async function loader({request, context}) {
-  return context.customerAccount.login();
+  const {cart, session} = context;
+  const formData = await request.formData();
+
+  const email = formData.get('email');
+  const password = formData.get('password');
+
+  try {
+    // Obtain a customer access token
+    // https://shopify.dev/docs/custom-storefronts/building-with-the-storefront-api/customer-accounts#step-3-create-an-access-token
+    const customerAccessToken = await doLogin(context, {email, password});
+    session.set('customerAccessToken', customerAccessToken);
+
+    // Sync customerAccessToken with existing cart
+    const result = await cart.updateBuyerIdentity({customerAccessToken});
+
+    // Update cart id in cookie
+    const headers = cart.setCartId(result.cart.id);
+
+    // Update session
+    headers.append('Set-Cookie', await session.commit());
+
+    return redirect(params.locale ? `/${params.locale}/account` : '/account', {
+      headers,
+    });
+  } catch (error) {
+    return 'something went wrong';
+  }
 }
 
 /** @typedef {import('@shopify/remix-oxygen').LoaderFunctionArgs} LoaderFunctionArgs */
